126d3c56a231e55b250ac03cf8fdad25ac30ee25
lppduy <lephamphuongduy@gmail.com>
2023-09-01 19:23:00 +0700

    2. Tạo Router cho ứng dụng

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CartPage from './CartPage/CartPage';
+import CheckoutPage from './CheckoutPage/CheckoutPage';
+import DetailPage from './DetailPage/DetailPage';
+import History from './History/History';
+import HomePage from './HomePage/HomePage';
+import LoginPage from './LoginPage/LoginPage';
+import RegisterPage from './RegisterPage/RegisterPage';
+import Footer from './Share/Footer/Footer';
+import Header from './Share/Header/Header';
+import ShopPage from './ShopPage/ShopPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={HomePage}
+          />{' '}
+          <Route
+            path="/detail/:id"
+            component={DetailPage}
+          />{' '}
+          <Route
+            path="/cart"
+            component={CartPage}
+          />{' '}
+          <Route
+            path="/login"
+            component={LoginPage}
+          />{' '}
+          <Route
+            path="/register"
+            component={RegisterPage}
+          />{' '}
+          <Route
+            path="/checkout"
+            component={CheckoutPage}
+          />{' '}
+          <Route
+            path="/history"
+            component={History}
+          />{' '}
+          <Route
+            path="/shop"
+            component={ShopPage}
+          />
+        </Switch>{' '}
+      </BrowserRouter>
+
+      <Footer />
     </div>
   );
 }
